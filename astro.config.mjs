7e74d34914c1bee0dd8e214001163037e7db1fd1
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2024-05-19 14:29:15 +0200

    chore: add astro config settings for correct resources paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://yurukyuruk.github.io',
+  base: 'air-conditioner-website',
   output: 'static',
 });
