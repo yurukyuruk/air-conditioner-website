313d62f2530db83071affd95c870f6d919f08cf9
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2024-05-19 15:57:30 +0200

    fix:: set domain name is astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yurukyuruk.github.io',
-  base: 'air-conditioner-website',
+  site: 'https://ms-klima-lodz.pl',
   output: 'static',
 });
